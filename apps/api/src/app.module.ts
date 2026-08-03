2c99420ac5329c625babc14dfbc096d77832c027
Александр Неудахин <alexneopro14@MacBookPro.localnet>
2026-08-03 07:52:39 +0300

    refactor(api): move ThrottlerModule.forRoot to AppModule, closes #29

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { MeetingsModule } from './meetings/meeting.module';
@@ -8,7 +9,19 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 @Module({
-  imports: [AuthModule, UserModule, MeetingsModule, FilesModule, PrismaModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: Number(process.env.THROTTLE_TTL_MS ?? 15 * 60 * 1000),
+        limit: Number(process.env.THROTTLE_LIMIT ?? 5),
+      },
+    ]),
+    AuthModule,
+    UserModule,
+    MeetingsModule,
+    FilesModule,
+    PrismaModule,
+  ],
   providers: [
     {
       provide: APP_FILTER,
